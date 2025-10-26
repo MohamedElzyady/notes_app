a701277056404dc566766ad56dc106cc2068a51b
Mohamed Elzyady <alzyadym920@gmail.com>
2025-10-26 21:08:03 +0300

    init hive and open note box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/home/home.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('note_box');
   runApp(const MyApp());
 }
 
