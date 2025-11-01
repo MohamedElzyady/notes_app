f480cfba411b7be8d2389d30f29c15fc3a73e4b9
Mohamed Elzyady <alzyadym920@gmail.com>
2025-11-02 00:15:42 +0200

    create fetch note cubit

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:note_app/constans/varuible.dart';
+import 'package:note_app/cubit/cubit/notes_cubit.dart';
 import 'package:note_app/home/widgets/custom_app_bar.dart';
 import 'package:note_app/home/widgets/custom_listView.dart';
 import 'package:note_app/home/widgets/floatingActionButton.dart';
@@ -10,19 +12,21 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton:Floatingactionbutton(),
-  
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 14),
-        child: Column(
-          children: [
-            Gap(70),
-            CustomAppPar(title: kvaruabil,),
-            Gap(20),
-            Expanded(child: CustomListview()),
-          ],
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Floatingactionbutton(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14),
+          child: Column(
+            children: [
+              Gap(70),
+              CustomAppPar(title: kvaruabil),
+              Gap(20),
+              Expanded(child: CustomListview()),
+            ],
+          ),
         ),
       ),
     );
